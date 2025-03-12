ce2d624b8761be71d4732ea0cddeb9540f661cb3
Roma <elusiuve15.09.93@icloud.com>
2025-03-12 15:08:44 +0200

    auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { GetUserRequest } from './get-user-req.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('get_user')
+  getUser(data: GetUserRequest) {
+    return this.appService.getUser(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { GetUserRequest } from './get-user-req.dto';
+
+export interface Users {
+  userId: string;
+  stripeUserId: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly users: Users[] = [
+    { userId: '123', stripeUserId: '44553' },
+    { userId: '456', stripeUserId: '46859' },
+    { userId: '678', stripeUserId: '84852' },
+  ];
+
+  getUser({ userId }: GetUserRequest) {
+    return this.users.find((user) => user.userId === userId);
   }
 }
